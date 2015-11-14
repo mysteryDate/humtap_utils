6b84327d9d3add66872c745ca6f58cf628b00aa3
Aaron Krajeski <ak@aaronk.is>
2015-11-14 10:57:36 -0500

    Add tempi distributions

--- bpm_graphs.py
+++ bpm_graphs.py
@@ -2,7 +2,7 @@ import json, pdb
 import plotly.plotly as plotly
 import plotly.graph_objs as go
 import numpy as np
-import operator
+import math
 
 MIN_TEMPO = 60
 MAX_TEMPO = 220
@@ -10,6 +10,17 @@ def main():
 
 	with open("genreArrFilenameFeatures.json", 'r') as infile:
 		data = json.load(infile)
+
+	with open("tempi_dist.txt", "r") as infile:
+		userTempos = infile.readlines()
+
+	nearest5s = [5 * int(math.floor(float(x)/5)) for x in userTempos]
+	tempoDistribution = {}
+	for tempo in nearest5s:
+		if tempo not in tempoDistribution:
+			tempoDistribution[tempo] = 0
+		tempoDistribution[tempo] += 1
+	# pdb.set_trace()
 
 	genre = "dance"
 	bpms = []
@@ -21,6 +32,7 @@ def main():
 		bpms.append(entry)
 
 	plotData = []
+	lineData = []
 	allx = [_ for _ in range(MIN_TEMPO, MAX_TEMPO + 1)]
 	ytotal = [0 for x in allx]
 	sortedBpms = sorted(bpms, key=lambda arr: arr[1][0])
@@ -28,15 +40,27 @@ def main():
 		ytotal = [arr[2] + y if x > arr[1][0] and x < arr[1][1] else y for (x,y) in zip(allx, ytotal)]
 		xvalues = [x for x in range(arr[1][0], arr[1][1] + 1)]
 		yvalues = [ytotal[x - MIN_TEMPO] for x in xvalues]
-		trace = go.Scatter(
+		lineTrace = go.Scatter(
 			x = xvalues,
 			y = yvalues,
 			mode = 'lines',
 			line = dict(width=1),
+			hoverinfo = 'name',
+			showlegend = False,
+			# fill = 'tonexty',
+			name = arr[0].split('.json')[0]
+		)
+		fill = go.Scatter(
+			x = allx,
+			y = ytotal,
+			mode = 'lines',
+			line = dict(width=0),
+			hoverinfo = 'none',
 			fill = 'tonexty',
 			name = arr[0].split('.json')[0]
 		)
-		plotData.append(trace)
+		plotData.append(fill)
+		plotData.append(lineTrace)
 
 	layout = go.Layout(
 		showlegend=True,
