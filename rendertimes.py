cac2eaab2dc024c4c8f1ef2b9d8282b1e7f829f5
Aaron Krajeski <ak@aaronk.is>
2015-11-18 18:49:12 -0500

    begin averaging arrangements, add genre data

--- rendertimes.py
+++ rendertimes.py
@@ -5,11 +5,13 @@ import plotly.graph_objs as go
 import numpy as np
 import pdb
 
-PATH_TO_DATA = "../data/electro_output.txt"
+PATH_TO_DATA = ["../data/electro_output.txt", "../data/rock_output.txt", "../data/hiphop_output.txt"]
 
 def loadData(path):
-    with open(path, "r") as infile:
-        data = json.load(infile)
+    data = []
+    for p in path:
+        with open(p, "r") as infile:
+            data = data + json.load(infile)
 
     return data
 
@@ -19,28 +21,47 @@ def main():
     arrangementData = {}
     for datum in rawData:
         if datum['arr'] not in arrangementData:
-            arrangementData[datum['arr']] = []
-        dataToAdd = {
-            'totalTime': datum['totalTime'],
-            'loopLength': datum['loopLength'],
-            'vsts': datum['vsts'],
-            'hum': datum['hum']
-        }
-        arrangementData[datum['arr']].append(dataToAdd)
+            # arrangementData[datum['arr']] = []
+            arrangementData[datum['arr']] = {
+                'totalTime': datum['totalTime'],
+                'loopLength': datum['loopLength'],
+                'vsts': datum['vsts'],
+                # 'hum': datum['hum']
+                'numRenders': 1
+            }
+        else:
+            arrangementData[datum['arr']]['totalTime'] += datum['totalTime']
+            arrangementData[datum['arr']]['loopLength'] += datum['loopLength']
+            arrangementData[datum['arr']]['vsts'] += datum['vsts']
+            arrangementData[datum['arr']]['numRenders'] += 1
+        # dataToAdd = {
+        #     'totalTime': datum['totalTime'],
+        #     'loopLength': datum['loopLength'],
+        #     'vsts': datum['vsts'],
+        #     'hum': datum['hum']
+        # }
+        # arrangementData[datum['arr']].append(dataToAdd)
 
     graphData = []
-    for arr in arrangementData:
-        trace = go.Scatter(
-            x = [d['vsts'] for d in arrangementData[arr]],
-            y = [d['totalTime'] for d in arrangementData[arr]],
-            mode = 'markers',
-            name = arr,
-            text = ["Hum:\t"+d['hum']+"<br>Length:\t"+str(round(float(d['loopLength'])/44100,2))+"s" for d in arrangementData[arr]]
-        )
-        graphData.append(trace)
+    # for arr in arrangementData:
+    #     trace = go.Scatter(
+    #         x = [d['vsts'] for d in arrangementData[arr]],
+    #         y = [d['totalTime'] for d in arrangementData[arr]],
+    #         mode = 'markers',
+    #         name = arr,
+    #         # text = ["Hum:\t"+d['hum']+"<br>Length:\t"+str(round(float(d['loopLength'])/44100,2))+"s" for d in arrangementData[arr]]
+    #     )
+    #     graphData.append(trace)
+    trace = go.Scatter(
+        x = [d['vsts']/d['numRenders'] for d in arrangementData.values()],
+        y = [d['totalTime']/d['numRenders'] for d in arrangementData.values()],
+        mode = 'markers',
+        text = [d for d in arrangementData]
+    )
+    graphData.append(trace)
 
     layout = go.Layout(
-        title='Render times by number of VSTs for electro',
+        title='Render times by number of VSTs by arrangement',
         hovermode='closest',
         xaxis=dict(
             title='Number of VSTs'
@@ -53,6 +74,6 @@ def main():
     py.sign_in("MysteryDate", "a6fd7sm5jr")
 
     fig = go.Figure(data=graphData, layout=layout)
-    plot_url = py.plot(fig, filename="Render times by vsts for electro (improved)")
+    plot_url = py.plot(fig, filename="Render times by vsts for all genres (averaged)")
 
 main()
